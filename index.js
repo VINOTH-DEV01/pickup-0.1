4a03c8680b8a427e0b50190eaa7e28a6753f5f9c
VINOTH-DEV01 <kumar434vinoth@gmail.com>
2021-05-16 23:36:43 +0530

    make it better

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const http = require("http");                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
 const app = require('./app');
 
-const port = process.env.port || 5000;
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.use("*", (req,res) => {
+                res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+const port = process.env.port || 5000;  
 const server = http.createServer(app);
 server.listen(port);
 
