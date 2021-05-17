13277a3f256fa3d540a25d35eba9831835c8dddb
VINOTH-DEV01 <kumar434vinoth@gmail.com>
2021-05-18 00:10:27 +0530

    make it better

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import LandingPage from  './pages/LandingPage/LandingPage';
+import Hello from  './components/Hello';
+
 function App() {
   return (
     <div className="App">
-      <LandingPage />
+      <Hello />
     </div>
   );
 }
