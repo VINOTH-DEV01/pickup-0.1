b539ff9d826dd5e48e6ac8222861d954045a5f24
VINOTH-DEV01 <kumar434vinoth@gmail.com>
2021-05-18 23:14:18 +0530

    fewmore changes are done!

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require("express");
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
     app.use("*", (req,res) => {
-                res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
 // const port = process.env.port || 5000;  
