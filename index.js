92d591a592b3c7e801fe33dd9ca399f892b3e860
VINOTH-DEV01 <kumar434vinoth@gmail.com>
2021-05-18 23:34:04 +0530

    first commit

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ if(process.env.NODE_ENV === 'production') {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
-// const port = process.env.port || 5000;  
-// const server = http.createServer(app);
-// server.listen(port);
+const port = process.env.port || 5000;  
+const server = http.createServer(app);
+server.listen(port);
 
      
 // server side configuration // 
-var port = process.env.PORT || 3000;
-const server = http.createServer(app);
-server.listen(port, "0.0.0.0", function() {
-console.log("Listening on Port 3000");
-});    
+// var port = process.env.PORT || 3000;
+// const server = http.createServer(app);
+// server.listen(port, "0.0.0.0", function() {
+// console.log("Listening on Port 3000");
+// });    
